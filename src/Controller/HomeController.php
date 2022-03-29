af3f34576e84d797ca0ad317df8b7c5621d7d3e4
ndiayemtr <ndiayemtr@gmail.com>
2022-03-29 15:30:17 +0000

    Afficher le formulaure de selection

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\City;
+use App\Entity\Country;
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('contry', EntityType::class, [
+                'placeholder' => 'Choice a country', 
+                'class' => Country::class,
+                'choice_label' => 'name',
+                'query_builder' => function(CountryRepository $countryRepo){
+                    return $countryRepo->createQueryBuilder('c')
+                            ->orderBy('c.name', 'ASC');
+                }
+             ] )
+            ->add('city', EntityType::class, [
+                'placeholder' => 'Choice a City', 
+                'class' => City::class,
+                'choice_label' => 'name',
+                'query_builder' => function(CityRepository $countryRepo){
+                    return $countryRepo->createQueryBuilder('c')
+                            ->orderBy('c.name', 'ASC');
+                }
+             ] )
+            ->add('message', TextareaType::class)
+            ->getForm();
+
+        return $this->render('home.html.twig',['form' => $form->createView() ]);
     }
 }
