6d0df5e4e0c9de265216734768d2d902e1b25f71
ndiayemtr <ndiayemtr@gmail.com>
2022-04-02 10:06:34 +0000

    FormEvents::PRE_SET_DATA

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\City;
 use App\Entity\Country;
+use Monolog\DateTimeImmutable;
 use App\Repository\CityRepository;
 use App\Repository\CountryRepository;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,49 +16,97 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Component\Validator\Constraints as Assert; 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, CountryRepository $countryRepos, CityRepository $cityRepos,  EntityManagerInterface $em): Response
     {
-        $form = $this->createFormBuilder()
+        //dd($countryRepos->find(1));
+        $form = $this->createFormBuilder(['country' => $countryRepos->find(1)])
             ->add('name', TextType::class,[
                 'constraints' => [
                     new NotBlank(['message' => 'Veuillez entrer votre nom'])
                 ],
             ])
-            ->add('contry', EntityType::class, [
+            ->add('age', IntegerType::class,[
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre age']),
+                    new Positive()
+                ],
+            ])
+            ->add('country', EntityType::class, [
                 'placeholder' => 'Choice a country', 
                 'class' => Country::class,
                 'choice_label' => 'name',
-                'query_builder' => function(CountryRepository $countryRepo){
-                    return $countryRepo->createQueryBuilder('c')
+                'query_builder' => function(CountryRepository $countryRepos){
+                    return $countryRepos->createQueryBuilder('c')
                             ->orderBy('c.name', 'ASC');
                 }
              ] )
-            ->add('city', EntityType::class, [
-                'placeholder' => 'Choice a City', 
-                'class' => City::class,
-                'choice_label' => 'name',
-                'query_builder' => function(CityRepository $countryRepo){
-                    return $countryRepo->createQueryBuilder('c')
-                            ->orderBy('c.name', 'ASC');
-                }
-             ] )
+             ->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $formEvent) use ($cityRepos){
+                dd($formEvent->getData());
+
+            })
+
+             ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $formEvent) use ($cityRepos){
+                $country = $formEvent->getData()['country'] ?? null;
+
+                $cities = $country === null ? [] :  $cityRepos->findBy(['country' => $country], ['name' => 'ASC']);
+
+                $formEvent->getForm()->add('city', EntityType::class, [
+                    'class' => City::class,
+                    'choice_label' => 'name',
+                    'choices' => $cities,
+                    'disabled' => $country === null,
+                    'placeholder' => 'Choice a City', 
+                    'constraints' => new NotBlank(['message' => 'Veuillez selectionner votre region']),
+                ]);
+
+            })
+
             ->add('message', TextareaType::class, [
+                'attr' => ['rows' => 5],
                 'constraints' => [
                     new NotBlank(['message' => 'Veuillez entrer votre text']),
                     new Length(['min' => 5])
                 ],
             ])
-            ->getForm();
+            ->add('availableAt', DateTimeType::class)
+            /* ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $formEvent) use ($cityRepos) {
+                  $country = $formEvent->getData()['country'] ?? null;
 
+                  dd($country);
+
+                if ($country !== null && $country->getId() == '1') {
+                    $id = $country->getId();
+                    $formEvent->getForm()->add('city', EntityType::class, [
+                        'placeholder' => 'Choice a City', 
+                        'class' => City::class,
+                        'choice_label' => 'name',
+                        'query_builder' => function() use ($cityRepos, $country){
+                            return $cityRepos->createQueryBuilder('c')
+                                    ->andWhere('c.country = :country')
+                                    ->setParameter('country', $country)
+                                    ->orderBy('c.name', 'ASC');
+                        }
+                     ] );
+                    //$formEvent->getForm()->remove('message');
+                } 
+            }) */
+
+            
         
+        ->getForm();
+        
+       
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
